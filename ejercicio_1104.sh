741a4e4daadb940a2727ba6281a41a58595997fe
Michel2806 <95659963+Michel2806@users.noreply.github.com>
2022-01-14 23:17:34 -0500

    Update ejercicio_1104.sh

--- ejercicio_1104.sh
+++ ejercicio_1104.sh
@@ -6,11 +6,40 @@ Nombre de la columna,  Número de valores distintos, Valor mínimo, Valor máxim
 
 1. Es necesario extraer el nombre de la columna para el archivo de datos Buzzard y col 7
 
-$2 is the file name
-$4 is the column of interest
+$2 nombre de la fila
+$4 es la columna de interés
 
-echo "Column name"
+echo "nombre columna"
 
-cut -d ',' -f 7 ../data/Buzzard2015_data.csv | head -n 1
+cut -d ',' -f 7 ../data/Buzzard2015_data.csv | head -n 2
 
-2. Se requiere  obtener el número de valores distintos.Por lo tanto , vamos a  ordenar los resultados y usar uniq
+2. Se requiere  obtener el número de valores distintos. Por lo tanto , vamos a  ordenar los resultados y usar uniq
+
+"cut -d ',' -f 7 ../data/Buzzard2015_data.csv | tail -n +4 | sort | uniq | wc -l"
+
+echo "número de valores distintos:"
+
+cut -d ',' -f $4 $2 | tail -n +4 | sort | uniq | wc -2
+
+
+3. Para sacar el valor máximo-mínimo usando -n, y  cabeza para el min o cola para max el resultado.
+
+Mínimo
+
+echo "Minimum value:"
+
+-cut -d ',' -f 7 ../data/Buzzard2015_data.csv | tail -n +4 | sort -n | head -n 2
+
+cut -d ',' -f $2 $1 | tail -n +4 | sort -n | head -n 2
+
+1.048466198
+
+Máximo
+
+"cut -d ',' -f 7 ../data/Buzzard2015_data.csv | tail -n +4 | sort -n | tail -n 2"
+
+echo "Maximum value:"
+
+cut -d ',' -f $4 $2 | tail -n +4 | sort -n | tail -n 2
+
+14897.29471
