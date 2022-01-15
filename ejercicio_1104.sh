0864655e8a6e7edfdd424576cc6b144d949a7ea3
Michel2806 <95659963+Michel2806@users.noreply.github.com>
2022-01-14 22:40:29 -0500

    Update ejercicio_1104.sh

--- ejercicio_1104.sh
+++ ejercicio_1104.sh
@@ -1,5 +1,6 @@
-# Escriba un script que, para un archivo csv dado y número de columna, imprime:
-## - Nombre de la columna
-## - Número de valores distintos
-## - Valor mínimo
-## - Valor máximo
+# GBI6_ExamenParcial1
+## Resuelva el ejercicio 1.10.4 Data Explorer ##
+Escriba un script que, para un archivo  y número de columna, imprime: 
+
+Nombre de la columna,  Número de valores distintos, Valor mínimo, Valor máximo 
+
